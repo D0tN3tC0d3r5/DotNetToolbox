6b5970e75cbfe44e29dcb4f9116ba0a00e5105d8
Andre Vianna <andrevianna@nureva.com>
2024-02-05 02:00:38 -0500

    Working on  Console

--- Source/PublishRelease.cmd
+++ Source/PublishRelease.cmd
@@ -3,16 +3,16 @@
 if [%1]==[] goto USAGE
 set target=%1
 
-call Publish %target% Core 8.0.4
-call Publish %target% ObjectDumper 8.0.4
-call Publish %target% TestUtilities 8.0.4
-call Publish %target% FluentAssertions 8.0.4
-call Publish %target% Console 8.0.4
+REM call Publish %target% Core 8.0.4
+REM call Publish %target% ObjectDumper 8.0.4
+REM call Publish %target% TestUtilities 8.0.4
+REM call Publish %target% FluentAssertions 8.0.4
+REM call Publish %target% Console 8.0.4
+REM call Publish %target% Http 8.0.4-rc1
+REM call Publish %target% OpenAI 8.0.4-rc1
 REM call Publish %target% ValidationBuilder 8.0.4-rc1
 REM call Publish %target% Azure 8.0.4-rc1
-REM call Publish %target% Http 8.0.4-rc1
 REM call Publish %target% Security 8.0.4-rc1
-REM call Publish %target% OpenAI 8.0.4-rc1
 goto :eof
 
 :USAGE
