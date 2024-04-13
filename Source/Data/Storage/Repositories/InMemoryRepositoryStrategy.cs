6cfa944eed4fdf6e11b16a6bf6a2989801b6f8c5
Andre Vianna <andre.vianna.rj@hotmail.com>
2024-04-13 00:10:46 -0400

    Working on Sophia

--- Source/Data/Storage/Repositories/InMemoryRepositoryStrategy.cs
+++ Source/Data/Storage/Repositories/InMemoryRepositoryStrategy.cs
@@ -14,33 +14,53 @@ public class InMemoryRepositoryStrategy<TItem>(IItemSet<TItem> repository)
         => (IItemSet<TResult>)ItemSet.Create(typeof(TResult), expression);
 
     public TResult ExecuteFunction<TResult>(string command, Expression? expression = null, object? input = null) {
-        var source = expression is LambdaExpression lambda ? Repository.Create(lambda) : Repository.Data;
+        var source = Repository.Data.AsQueryable();
         object? result = command switch {
-            "Any" when typeof(TResult) == typeof(bool) => source.Cast<object?>().Any(),
-            "Count" when Repository.Data is ICollection<TItem> c && typeof(TResult) == typeof(int) => source.Cast<object?>().Count(),
-            "Create" when typeof(TItem).IsClass && Repository.Data is ICollection<TItem> c && input is Action<TItem> set && typeof(TResult) == typeof(TItem) => CreateSetAndAddItem(c, set),
-            "ElementAt" when input is int index => source.Cast<TItem>().ElementAt(index),
-            "FindFirst" when typeof(TResult) == typeof(TItem) => Repository.Data.FirstOrDefault(),
-            "GetList" when typeof(TResult) == typeof(TItem[]) => Repository.Data.ToArray(),
-            _ => throw new NotSupportedException($"Command '{command}' is not supported for a '{Repository.GetType().Name}<{typeof(TItem).Name}>'."),
+            "Any" when expression is not null => Repository.Data.Any(ToDelegate<Func<TItem, bool>>(expression)),
+            "Any" => Repository.Data.Any(),
+            "Count" when expression is not null => Repository.Data.Count(ToDelegate<Func<TItem, bool>>(expression)),
+            "Count" => Repository.Data.Count(),
+            "FindFirst" when expression is not null => Repository.Data.FirstOrDefault(ToDelegate<Func<TItem, bool>>(expression)),
+            "FindFirst" => Repository.Data.FirstOrDefault(),
+
+            "GetList" => Repository.Data.ToArray(),
+            "ElementAt" when input is int index => Repository.Data.ElementAt(index),
+
+            "Create" when typeof(TItem).IsClass && input is Action<TItem> set => CreateSetAndAddItem(set),
+            _ => throw new NotImplementedException($"Command '{command}' is not implemented for '{Repository.GetType().Name}<{typeof(TItem).Name}>'."),
         };
         return IsOfTypeOrDefault<TResult>(result)!;
+    }
+
+    private TResult ToDelegate<TResult>(Expression expression)
+        where TResult : Delegate {
+        try {
+            if (expression is LambdaExpression lambda)
+                return (TResult)lambda.Compile(true);
+            throw new NotImplementedException($"Command expression '{expression}' is not implemented for '{Repository.GetType().Name}<{typeof(TItem).Name}>'.");
+        } catch (Exception ex) {
+            throw new NotSupportedException($"Command expression '{expression}' is not supported for '{Repository.GetType().Name}<{typeof(TItem).Name}>'.", ex);
+        }
     }
 
     public void ExecuteAction(string command, Expression? expression = null, object? input = null) {
         var source = expression is LambdaExpression lambda ? Repository.Create(lambda) : Repository.Data;
         switch (command) {
-            case "Add" when Repository.Data is ICollection<TItem> c && input is TItem i:
-                c.Add(i);
+            case "Add" when input is TItem i:
+                DataAsCollection.Add(i);
                 return;
         }
         throw new NotSupportedException($"Command '{command}' is not supported.");
     }
 
-    private static TItem CreateSetAndAddItem(ICollection<TItem> collection, Action<TItem> set) {
+    private ICollection<TItem> DataAsCollection
+        => Repository.Data as ICollection<TItem>
+        ?? throw new NotSupportedException("Repository data is not a collection.");
+
+    private TItem CreateSetAndAddItem(Action<TItem> set) {
         var item = Activator.CreateInstance<TItem>();
         set(item);
-        collection.Add(item);
+        DataAsCollection.Add(item);
         return item;
     }
 }
