822d7e46a93b2d7c9df1ad50902308ed6f84f7c6
Andre Vianna <andre.vianna.rj@hotmail.com>
2024-05-21 15:01:32 -0400

    Prepare release

--- Source/PublishAll.cmd
+++ Source/PublishAll.cmd
@@ -14,9 +14,9 @@ call Publish %target% Utilities\ValidationBuilder ValidationBuilder 8.1.0
 call Publish %target% Cloud\Azure Azure 8.1.0
 call Publish %target% Cloud\Http Http 8.1.0
 call Publish %target% Utilities\Security Security 8.1.0
-call Publish %target% AI\Core AI 8.1.0
-call Publish %target% AI\Anthropic AI.Anthropic 8.1.0
-call Publish %target% AI\OpenAI AI.OpenAI 8.1.0
+call Publish %target% AI\Core AI 8.1.0-rc1
+call Publish %target% AI\Anthropic AI.Anthropic 8.1.0-rc1
+call Publish %target% AI\OpenAI AI.OpenAI 8.1.0-rc1
 goto :eof
 
 :USAGE
--- Source/PublishSome.cmd
+++ Source/PublishSome.cmd
@@ -3,20 +3,20 @@
 if [%1]==[] goto USAGE
 set target=%1
 
-call Publish %target% Core\Core Core 8.1.0
-call Publish %target% Core\Linq Linq 8.1.0
-call Publish %target% Data\Storage Data 8.1.0
-call Publish %target% Utilities\ObjectDumper ObjectDumper 8.1.0
-call Publish %target% Testing\TestUtilities TestUtilities 8.1.0
-call Publish %target% Testing\FluentAssertions FluentAssertions 8.1.0
-call Publish %target% Utilities\ValidationBuilder ValidationBuilder 8.1.0
-call Publish %target% Cloud\Azure Azure 8.1.0
-call Publish %target% Cloud\Http Http 8.1.0
-call Publish %target% Utilities\Security Security 8.1.0
-call Publish %target% Applications\Console Console 8.1.0
-REM call Publish %target% AI\Core AI 8.1.0
-REM call Publish %target% AI\Anthropic AI.Anthropic 8.1.0
-REM call Publish %target% AI\OpenAI AI.OpenAI 8.1.0
+REM call Publish %target% Core\Core Core 8.1.0
+REM call Publish %target% Core\Linq Linq 8.1.0
+REM call Publish %target% Data\Storage Data 8.1.0
+REM call Publish %target% Utilities\ObjectDumper ObjectDumper 8.1.0
+REM call Publish %target% Testing\TestUtilities TestUtilities 8.1.0
+REM call Publish %target% Testing\FluentAssertions FluentAssertions 8.1.0
+REM call Publish %target% Utilities\ValidationBuilder ValidationBuilder 8.1.0
+REM call Publish %target% Cloud\Azure Azure 8.1.0
+REM call Publish %target% Cloud\Http Http 8.1.0
+REM call Publish %target% Utilities\Security Security 8.1.0
+REM call Publish %target% Applications\Console Console 8.1.0
+call Publish %target% AI\Core AI 8.1.0-rc1
+call Publish %target% AI\Anthropic AI.Anthropic 8.1.0-rc1
+call Publish %target% AI\OpenAI AI.OpenAI 8.1.0-rc1
 goto :eof
 
 :USAGE
