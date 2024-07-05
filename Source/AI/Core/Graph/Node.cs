9f306697114c26d8ac0823932909de7d801f6b61
Andre Vianna <andre.vianna.rj@hotmail.com>
2024-07-05 16:05:16 -0400

    graph

---
+++ Source/AI/Core/Graph/Node.cs
@@ -0,0 +1,44 @@
+﻿namespace DotNetToolbox.AI.Graph;
+
+public abstract class Runner(Node entry, IDisposable state)
+    : IEnumerator<Node> {
+    private bool _isValueDisposed;
+
+    public IDisposable State { get; } = state;
+
+    public Node Current { get; } = entry;
+    object IEnumerator.Current => Current;
+
+    public bool MoveNext() => throw new NotImplementedException();
+    public void Reset() => throw new NotImplementedException();
+
+    protected virtual void Dispose(bool disposing) {
+        if (_isValueDisposed)
+            return;
+        if (disposing) {
+            State.Dispose();
+        }
+        _isValueDisposed = true;
+    }
+
+    public void Dispose() {
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+}
+
+public abstract record End(object? Value = null) : NodeResult(Value) {
+}
+
+public abstract record NodeResult(object? Value = null, Node? Next = null) {
+}
+
+public abstract class Node(string id, string name) {
+    public string Id { get; set; } = id;
+    public string Name { get; set; } = name;
+    public List<Node> Edges { get; set; } = [];
+
+    public abstract Task<NodeResult> Run(object input);
+
+    public override string ToString() => Name;
+}
--- Source/Console.UnitTests/TestDoubles/TestFileProvider.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DotNetToolbox.ConsoleApplication.TestDoubles;
-
-public class TestFileProvider
-    : IFileProvider {
-    public IFileInfo GetFileInfo(string subpath) => new TestFileInfo();
-    public IDirectoryContents GetDirectoryContents(string subpath) => throw new NotImplementedException();
-    public IChangeToken Watch(string filter) => new TestChangeToken();
-}
