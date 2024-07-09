51a577f636d574ca2d90039a06449cbf4ace58a8
Andre Vianna <andre.vianna.rj@hotmail.com>
2024-07-09 16:12:54 -0400

    graph

---
+++ Source/Core/Graph/GlobalUsings.cs
@@ -0,0 +1,28 @@
+// Global using directives
+
+global using System.Collections;
+global using System.Net;
+global using System.Net.Http.Headers;
+global using System.Net.Http.Json;
+global using System.Runtime.CompilerServices;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+
+global using DotNetToolbox.AI.Agents;
+global using DotNetToolbox.AI.Chats;
+global using DotNetToolbox.AI.Common;
+global using DotNetToolbox.AI.Consumers;
+global using DotNetToolbox.AI.Extensions;
+global using DotNetToolbox.AI.Personas;
+global using DotNetToolbox.Environment;
+global using DotNetToolbox.Http;
+global using DotNetToolbox.Http.Extensions;
+global using DotNetToolbox.Results;
+global using DotNetToolbox.Threading;
+
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.DependencyInjection.Extensions;
+global using Microsoft.Extensions.Logging;
+
+global using static System.Ensure;
