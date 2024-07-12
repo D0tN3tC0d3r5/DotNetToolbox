d7597a5be9ffac4bc4ce5a4d9afeea19a558838d
Andre Vianna <andre.vianna.rj@hotmail.com>
2024-07-12 16:55:31 -0400

    graph

--- Source/Core/Graph.UnitTests/PathBuilder/PathStarterTests.cs
+++ Source/Core/Graph.UnitTests/PathBuilder/PathStarterTests.cs
@@ -10,20 +10,27 @@ public class PathBuilderTests {
 
     [Fact]
     public void Then_WithAction_SetsTruePath() {
-        var path = Start.If(_ => true).Then(_ => { });
+        var path = Start.If(_ => true).Then(_ => End);
+        path.Should().NotBeNull();
+        path.Should().BeAssignableTo<IElseBuilder>();
+    }
+
+    [Fact]
+    public void Then_WithNode_SetsTruePath() {
+        var path = Start.If(_ => true).Then(End);
         path.Should().NotBeNull();
         path.Should().BeAssignableTo<IElseBuilder>();
     }
 
     [Fact]
     public void Else_WithNode_SetsFalsePath() {
-        var path = Start.If(_ => false).Then(_ => { }).Else(_ => { });
+        var path = Start.If(_ => false).Then(_ => End).Else(_ => End);
         path.Should().NotBeNull();
         path.Should().BeAssignableTo<IPathBuilder>();
     }
     [Fact]
     public void ElseIf_WithPredicate_AddsElseIfNode() {
-        var path = Start.If(_ => false).Then(_ => { }).ElseIf(_ => true);
+        var path = Start.If(_ => false).Then(_ => End).ElseIf(_ => true);
         path.Should().NotBeNull();
         path.Should().BeAssignableTo<IThenBuilder>();
     }
